393afb92dad5de5f29fca7ec86dc44b0c9797f43
arduinocc04 <arduinocc04@gmail.com>
2020-11-19 22:08:33 +0900

    cmd added in setup

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ import DbManager
 
 if __name__ == '__main__':
     if(not os.path.isdir('./static/image')): os.mkdir('./static/image')
-    if(os.path.isfile('./Data.db')): os.remove('./Data.db')
-    DbMan = DbManager.DbManager()
-    DbMan.makeDb()
-    DbMan.closeDb()
+    cmd = int(input("Del Db? yes->1 no->0"))
+    if cmd == 1:
+        if(os.path.isfile('./Data.db')): os.remove('./Data.db')
+        DbMan = DbManager.DbManager()
+        DbMan.makeDb()
+        DbMan.closeDb()
+    
