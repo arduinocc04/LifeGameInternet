38bb2be7a9701cfe69f260b5469eac8fe828b04e
arduinocc04 <arduinocc04@gmail.com>
2020-11-19 22:01:05 +0900

    commit

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ import os
 import DbManager
 
 if __name__ == '__main__':
-    os.mkdir('./static/image')
-    if(os.path.isfile('./Data.db')):
-        os.remove('./Data.db')
+    if(not os.path.isdir('./static/image')): os.mkdir('./static/image')
+    if(os.path.isfile('./Data.db')): os.remove('./Data.db')
     DbMan = DbManager.DbManager()
     DbMan.makeDb()
     DbMan.closeDb()
